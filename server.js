a7e41c9cb9ece189ab96b7c273d0f416549e9827
rwNDevS <xrw9sx@gmail.com>
2026-06-19 04:06:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve('./public')));
+
+const publicDir = path.join(__dirname, 'public');
+
+app.use(express.static(publicDir));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicDir, 'index.html'));
+});
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(publicDir, 'index.html'));
+});
+
+app.get('/admin.html', (req, res) => {
+    res.sendFile(path.join(publicDir, 'admin.html'));
+});
 
 const PORT = process.env.PORT || 8040;
 
